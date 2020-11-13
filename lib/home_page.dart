a23d468b44e9eb84a076d04efddbb4bf5a7d4e49
bonzoman <37900414+bonzoman@users.noreply.github.com>
2020-11-13 14:36:30 +0900

    111111111

---
+++ lib/home_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Instagram Clon",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Text("Instagram에 오신걸 환영합니다.", style: TextStyle(fontSize: 24.0)),
+                Padding(padding: EdgeInsets.all(8.0)),
+                Text("사진과 동영상을 보려면 팔로우 하세요."),
+                Padding(padding: EdgeInsets.all(8.0)),
+                SizedBox(
+                  width: 260.0,
+                  child: Card(
+                    elevation: 4.0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Padding(padding: EdgeInsets.all(4.0)),
+                          SizedBox(
+                            width: 80.0,
+                            height: 80.0,
+                            child: CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                    'https://cdn.upbit.com/images/sliderbt01.34aab91.png')),
+                          ),
+                          Padding(padding: EdgeInsets.all(8.0)),
+                          Text('이메일 주소',
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text('이름'),
+                          Padding(padding: EdgeInsets.all(8.0)),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                width: 70,
+                                height: 70,
+                                child: Image.network(
+                                  "https://static.upbit.com/logos/SRN.png",
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Padding(padding: EdgeInsets.all(1.0)),
+                              SizedBox(
+                                width: 70,
+                                height: 70,
+                                child: Image.network(
+                                  "https://static.upbit.com/logos/THETA.png",
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Padding(padding: EdgeInsets.all(1.0)),
+                              SizedBox(
+                                width: 70,
+                                height: 70,
+                                child: Image.network(
+                                  "https://static.upbit.com/logos/SRN.png",
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Padding(padding: EdgeInsets.all(4.0)),
+                          Text('Facebook 친구'),
+                          Padding(padding: EdgeInsets.all(4.0)),
+                          RaisedButton(
+                            child: Text("팔로우"),
+                            color: Colors.blueAccent,
+                            textColor: Colors.white,
+                            onPressed: () {},
+                          ),
+                          Padding(padding: EdgeInsets.all(4.0)),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/tab_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/home_page.dart';
+
+class TabPage extends StatefulWidget {
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0;
+  List _pages = [HomePage(), Text("page2"), Text("page3")];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _pages[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+          fixedColor: Colors.black,
+          onTap: _onItemTapped,
+          currentIndex: _selectedIndex,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle), label: "Account")
+          ]),
+    );
+  }
+
+  void _onItemTapped(int value) {
+    setState(() {
+      _selectedIndex = value;
+    });
+  }
+}
