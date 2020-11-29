f30b7e9d12d17c752376c2f18ba0a69357a3be69
오승준 <ohseungjun@oseungjun-ui-MacBook-Pro.local>
2020-11-29 13:17:30 +0900

    44444

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -24,7 +24,7 @@ class HomePage extends StatelessWidget {
               children: [
                 Text("Instagram에 오신걸 환영합니다.", style: TextStyle(fontSize: 24.0)),
                 Padding(padding: EdgeInsets.all(8.0)),
-                Text("사진과 동영상을 보려면 팔로우 하세요..."),
+                Text("사진과 동영상을 보려면 팔로우 하세요...."),
                 Padding(padding: EdgeInsets.all(8.0)),
                 SizedBox(
                   width: 260.0,
